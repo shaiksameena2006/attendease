319e302ed9804fecaff49ff386b2a632e5f3a7e4
shaiksameena2006 <shaiksameena2k06@gmail.com>
2025-11-10 11:42:50 +0530

    auto-commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
+    port: 8081, // make sure frontend runs on 8081
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5001", // Flask backend
+        target: "http://localhost:5001",
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
 
 
