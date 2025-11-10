80c2ce48459860d2f85481f757c2f333175e0e8a
shaiksameena2006 <shaiksameena2k06@gmail.com>
2025-11-10 11:58:46 +0530

    auto-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8081, // make sure frontend runs on 8081
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
-        secure: false,
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 8081, // or whatever port you want
+  },
 });
-
 
