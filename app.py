537baa7c3e6980a8b6d17e7dbacff45d7ca9d4e8
shaiksameena2006 <shaiksameena2k06@gmail.com>
2025-11-12 10:09:31 +0530

    auto-commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
-# Tell Flask exactly where to find the templates
-app = Flask(__name__, template_folder="template/templates")
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def home():
