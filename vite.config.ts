1cbb704380f0131914ca88328de74126d0452a27
shaiksameena2006 <shaiksameena2k06@gmail.com>
2026-03-28 11:04:23 +0530

    auto-commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  root: ".", // 👈 Your main folder (attendease)
+   // 👈 Your main folder (attendease)
   publicDir: "public",
   server: {
     host: "::",
