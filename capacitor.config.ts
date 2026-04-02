1a81e4f495e3f9aa9acd0562a61504648a3e0765
shaiksameena2006 <shaiksameena2k06@gmail.com>
2026-04-02 23:39:07 +0530

    auto-commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.attendease.app',
+  appName: 'attendease',
+  webDir: 'dist'
+};
+
+export default config;
